e496e2e5f60f31d4206a06238eada7f168a6739c
hoodielive <hoodielive@gmail.com>
2018-08-19 13:55:48 -0400

    scripting and notes

--- bash/tests.sh
+++ bash/tests.sh
@@ -2,12 +2,15 @@
 
 set -x
 
+# test 1
 if [[ "abc" == "abc" ]] 
 then
 	echo "yep" 
 else
 	echo "nope"
 fi
+
+# test 2
 
 if [[ string1 == "string1" ]] 
 then 
@@ -16,12 +19,16 @@ else
 	echo "they are not the same"
 fi 
 
+# test 3
+
 if [[ "abc" == "c" ]] 
 then
 	echo "yep" 
 else
 	echo "nope"
 fi
+
+# test 4
 
 if [[ "abc" == *c ]] 
 then 
@@ -30,6 +37,8 @@ else
 	echo "nope"
 fi 
 
+# test 5
+
 if [[ "linuxfestnorthewst 2018" == [a-z]*[^[:digit:]] ]]
 then
 	echo "doesn't match anything"
@@ -37,6 +46,7 @@ else
 	echo "matches something"
 fi
 
+# test 6
 if [[ "linuxfestnorthewst 2018" == [a-z]*[[:digit:]] ]]
 then
 	echo "doesn't match anything"
@@ -45,9 +55,33 @@ else
 fi
 
 
+# test 7
+
 if [[ "linuxfestnorthwest 2017" != *[^[:digit:]]* ]] 
 then
 	echo "oops"
 else
 	echo "ouch"
 fi
+
+# test 8 
+
+case one in 
+	o) 
+		echo 'o' 
+		;; 
+	o?e) 
+		echo 'o?e'
+		;;
+	o*) 
+		echo 'o*'
+		;;
+	*)
+		echo 'nope'
+		;; 
+esac
+
+# test 9 parameters 
+
+set -- one two "three four" five 
+printf "%s\n" "\$1: $1" "\$2: $2" "\$3: $3" "\$4: $4" "\$5: $5" "\$#: $#" "\$*: $*" "\$@: $@" 
