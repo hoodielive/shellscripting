aa1728eb20ee8f4d4e5c195d87953a20bb4ef060
hoodielive <hoodielive@gmail.com>
2018-08-19 13:12:08 -0400

    tests and notes

--- bash/tests.sh
+++ bash/tests.sh
@@ -1,1 +1,53 @@
+#!/bin/bash 
 
+set -x
+
+if [[ "abc" == "abc" ]] 
+then
+	echo "yep" 
+else
+	echo "nope"
+fi
+
+if [[ string1 == "string1" ]] 
+then 
+	echo "they are the same"
+else
+	echo "they are not the same"
+fi 
+
+if [[ "abc" == "c" ]] 
+then
+	echo "yep" 
+else
+	echo "nope"
+fi
+
+if [[ "abc" == *c ]] 
+then 
+	echo "yep"
+else
+	echo "nope"
+fi 
+
+if [[ "linuxfestnorthewst 2018" == [a-z]*[^[:digit:]] ]]
+then
+	echo "doesn't match anything"
+else 
+	echo "matches something"
+fi
+
+if [[ "linuxfestnorthewst 2018" == [a-z]*[[:digit:]] ]]
+then
+	echo "doesn't match anything"
+else 
+	echo "matches something"
+fi
+
+
+if [[ "linuxfestnorthwest 2017" != *[^[:digit:]]* ]] 
+then
+	echo "oops"
+else
+	echo "ouch"
+fi
