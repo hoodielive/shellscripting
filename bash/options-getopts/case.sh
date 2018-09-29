4315d2d4024a4d4805621e0e1a884a3b15795187
hoodielive <hoodielive@gmail.com>
2018-09-29 10:46:01 -0400

    case statement

---
+++ bash/options-getopts/case.sh
@@ -0,0 +1,31 @@
+#!/bin/bash 
+
+#if [[ "${1}" = 'start' ]]
+#then
+#  echo "Starting..."
+#elif [[ "${1}" = 'stop' ]]
+#then
+#  echo "Stopping..."
+#elif [[ "${1}" = 'status' ]]
+#then
+#  echo "Status..." 
+#else
+#  echo "Please enter a valid option"
+#  exit 44
+#fi
+
+case "${1}" in
+  start) 
+    echo "Starting..."
+    ;; 
+  stop) 
+    echo "Stopping..."
+    ;; 
+  status) 
+    echo "Status is as follows: "
+    ;;
+  *) 
+    echo "Follow the rules!"
+    exit 44
+    ;;
+esac
