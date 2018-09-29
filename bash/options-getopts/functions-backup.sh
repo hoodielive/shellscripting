c6570a8f92e89379bed8e86bbd6036ef5f45c327
hoodielive <hoodielive@gmail.com>
2018-09-29 11:57:56 -0400

    perform backups

--- bash/options-getopts/functions-backup.sh
+++ bash/options-getopts/functions-backup.sh
@@ -27,10 +27,37 @@ log() {
   logger -t functions-readonly.sh "${MESSAGE}"
 }
 
+backup_file() {
+  # this function creates a backup of a file. Returns a non-zero status on error
+  local FILE="${1}"
+  
+  # Make sure the file exists 
+  if [[ -f "${FILE}" ]]
+  then
+    local BACKUP_FILE="/var/tmp/$(basename ${FILE}).$(date +%F-%N)"
+    log "Backing up ${FILE} to ${BACKUP_FILE}."
+    # the exit status of the function will be the exit status of the cp command
+    cp -p ${FILE} ${BACKUP_FILE}
+  else
+    # The file does not exist, so return a non-zero exit status 
+    return 1 
+  fi
+}
+
 # readonly is like a constant in other languages - makes the var unchangeable 
-#readonly VERBOSE='true'
-readonly VERBOSE='false'
+readonly VERBOSE='true'
 log 'This is the message I have for you'
 log 'this is fun!'  
 
 # Logger is a shell command interface to the syslog(3) system log module 
+
+backup_file '/etc/passwd'
+
+# Make a decision based on the exit status of the function 
+if [[ "${?}" -eq '0' ]]
+then
+  log 'File backup succeeded!'
+else
+  log 'File backup failed!'
+  exit 1
+fi
