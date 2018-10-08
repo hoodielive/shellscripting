5225aa9cdd1af782d78a82cf90e402646bb74916
hoodielive <hoodielive@gmail.com>
2018-10-07 21:43:56 -0400

    compliance for dconf psuedo_code_draft

---
+++ bash/nfs_dconf/compliance.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+
+# check to see if any users are logged in
+# cd into the directories 
+# check for the existence of file 
+# if file exists it meets compliance then log it and make a report
+# if not then the check does not meet compliance and should die 
+
+# if compliance passes then dump the dconf user settings
+# the script should die.. make change to true in puppet and wait
+
