c200442872c36f034a1b6a36aae39e14248376bf
hoodielive <hoodielive@gmail.com>
2019-01-26 13:45:06 -0500

    who is hogging the damn disk

---
+++ python/who_is_hogging_the_disk.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3 
+
+""" a script to see who's hogging shared space """
+
+import sys 
+
+def get_totals(rows):
+    """ function to find space used by users in a ls -1 listing rows """
+    users = {}
+    for row in rows:
+        if row.startswithd('-'):
+            fields = row.split()
+            username, filesize = fields[2], fields[4]
+            if username not in users:
+                users[username] = 0
+            users[username] += int(filesize)
+    userlist = users.items()
+    userlist.sort(cmp=lambda x,y: cmp(x[1], y[1]), reverse=True)
+    return userlist
