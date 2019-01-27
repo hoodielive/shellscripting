d5825782bb78f1f17379bf16cdf4cf7bc48fba3a
hoodielive <hoodielive@gmail.com>
2019-01-26 21:37:53 -0500

    disk hogs

--- python/who_is_hogging_the_disk.py
+++ python/who_is_hogging_the_disk.py
@@ -8,12 +8,15 @@ def get_totals(rows):
     """ function to find space used by users in a ls -1 listing rows """
     users = {}
     for row in rows:
-        if row.startswithd('-'):
+        if row.startswith('-'):
             fields = row.split()
             username, filesize = fields[2], fields[4]
             if username not in users:
                 users[username] = 0
             users[username] += int(filesize)
     userlist = users.items()
+    # fix this because dictionary does NOT have a sort attribute
     userlist.sort(cmp=lambda x,y: cmp(x[1], y[1]), reverse=True)
     return userlist
+
+get_totals('df')
