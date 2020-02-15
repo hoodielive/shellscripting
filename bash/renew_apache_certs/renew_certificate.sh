2995cea3b01ae9bc5e4de1c81de96b83067bfad7
hoodielive <hoodielive@gmail.com>
2020-02-14 19:01:37 -0500

    Lets see..

--- bash/renew_apache_certs/renew_certificate.sh
+++ bash/renew_apache_certs/renew_certificate.sh
@@ -9,6 +9,7 @@ unset NEW_CSR
 unset NEW_KEY
 unset NEW_BUNDLE
 
+DATE=$(date)
 OLD_CERT=0
 OLD_CSR=0
 OLD_KEY=0
@@ -20,9 +21,77 @@ NEW_BUNDLE=0
 
 ARG=""
 
-case $ARG in
-    1)
-        cert_renewal=1
-        break
-        ;;
-    2)
+# Functions
+log_begin_msg_pre() { :; }
+log_begin_msg_post() { :; }
+
+log_warning_msg () {
+    if [ -n "${1:-}" ]; then
+        log_begin_msg $@ "..."
+    fi
+    log_end_msg 255 || true
+}
+
+log_begin_msg() {
+	log_begin_msg_pre "$@"
+	if [ -z "${1:-}" ]; then
+		return 1
+	fi
+	echo -n "$@" || true
+	log_begin_msg_post "$@"
+}
+
+figure_out_year() {
+	echo -n "What year is the renewal for?"
+	read -p NEW_YEAR
+}
+
+OPTIND=1
+while getopts rqh:p: OPT ; do
+	case $OPT in
+    r) renew_cert=$OPTARG ;; 
+    q) query_cert=$OPTARG ;;
+		h) get_help=$OPTARG ;;
+		default)
+			help ;;
+	esac
+done
+shift $(( $OPTIND - 1 ))
+
+if [ ${OPTARG} == "r" ]; then
+	echo -n "Cert renewal for which domain?"
+	read -p DOMAIN
+
+	echo -n "What year was the present certificate issued?"
+	read -p OLD_YEAR
+
+	echo -n "And you are attempting to renew cert for year ${DATE}"
+	read -p NEW_YEAR_ANSWER
+else
+	log_warning_msg "Error: Please check your entries as something has went awfully wrong."
+fi
+
+	if [ $NEW_YEAR_ANSWER == "yes" ]; then
+		NEW_YEAR=${DATE}
+	else
+		figure_out_year
+	fi
+
+	echo -n "Is ${DOMAIN} the correct domain for cert renewal? yes or no"
+	read -p ANSWER
+
+	if [ ${ANSWER} == "yes" ]; then
+			# Decommission old certificates.
+			unlink /etc/pki/tls/private/${DOMAIN_KEY}.${OLD_YEAR}.key
+			unlink /etc/pki/tls/private/${DOMAIN_CSR}.${OLD_YEAR}.csr
+			unlink /etc/pki/tls/certs/${DOMAIN}.${OLD_YEAR}.crt
+			unlink /etc/pki/tls/certs/${DOMAIN_BUNDLE}.${OLD_YEAR}.crt
+
+			# Put new year certs in place.
+			ln -s /etc/pki/tls/private/${DOMAIN_KEY}.${NEW_YEAR}.key /etc/pki/tls/private/${DOMAIN_KEY}.key
+			ln -s /etc/pki/tls/private/${DOMAIN_CSR}.${NEW_YEAR}.csr /etc/pki/tls/private/${DOMAIN_CSR}.csr
+			ln -s /etc/pki/tls/certs/${DOMAIN}.${NEW_YEAR}.crt /etc/pki/tls/certs/${DOMAIN}.crt
+			ln -s /etc/pki/tls/certs/${DOMAIN_BUNDLE}.${NEW_YEAR}.crt /etc/pki/tls/certs/${DOMAIN_BUNDLE}.crt
+		else
+			log_warning_msg "Error: Please check your requirements."
+	fi
