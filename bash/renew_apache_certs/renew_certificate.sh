fcb148546dfbd19f28f6a8da678332162f737c4d
hoodielive <hoodielive@gmail.com>
2020-02-17 19:36:42 -0500

    remove cruft

--- bash/renew_apache_certs/renew_certificate.sh
+++ bash/renew_apache_certs/renew_certificate.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-unset OLD_CERT
-unset OLD_CSR
-unset OLD_KEY
-unset OLD_BUNDLE
-unset NEW_CERT
-unset NEW_CSR
-unset NEW_KEY
-unset NEW_BUNDLE
+set -x
 
-DATE=$(date +"%Y") # Parse year from date command.
-OLD_CERT=0
-OLD_CSR=0
-OLD_KEY=0
-OLD_BUNDLE=0
-NEW_CERT=0
-NEW_CSR=0
-NEW_KEY=0
-NEW_BUNDLE=0
+unset THE_DOMAIN
+unset NEW_YEAR
+
+
 
 # Functions
 log_begin_msg_pre() { :; }
@@ -53,8 +41,8 @@ OPTIND=1
 while getopts drqh: OPT ; do
 	case $OPT in
 		-d) DEBUG=1 ;;
-    -r) RENEW_CERT=1 ;; 
-    -q) QUERY_CERT=1 ;;
+		-r) RENEW_CERT=1 ;; 
+		-q) QUERY_CERT=1 ;;
 		-h) GET_HELP=1 ;;
 		default)
 			help ;;
@@ -71,40 +59,35 @@ fi
 
 # @Cert Renewal?
 if [ "${OPTARG} " == "r" ]; then
-	echo "Cert renewal for which domain? "
-	read DOMAIN
+	echo "For what domain are you renewing?"
+	read THE_DOMAIN
 
-	echo "What year was the present certificate issued? "
-	read OLD_YEAR
-
-	echo "And you are attempting to renew cert for year ${DATE} "
-	read NEW_YEAR_ANSWER
+	echo "For what year is this renewal?"
+	read NEW_YEAR
 else
 	echo "Moving on.."
 	#log_warning_msg "Error: Please check your entries as something has went awfully wrong."
 fi
 
-echo "Is ${DOMAIN} the correct domain for cert renewal? yes or no "
-read ANSWER
+# We should set permissions on new certificates, csrs and keys.
+chmod 0644 /etc/pki/tls/private/{DOMAIN}.${NEW_YEAR}.key
+chmod 0644 /etc/pki/tls/private/{DOMAIN}.${NEW_YEAR}.csr
+chmod 0644 /etc/pki/tls/certs/{DOMAIN}.${NEW_YEAR}.crt
+chmod 0644 /etc/pki/tls/private/{SHORTNAME}-ca-bundle.${NEW_YEAR}.crt
 
-if [ "${NEW_YEAR_ANSWER}" == "yes" ]; then
-	NEW_YEAR=${DATE}
-else
-	figure_out_year
-fi
 
-if [ "${ANSWER}" == "yes" ]; then
-		# @Decommission old certificates.
-		unlink /etc/pki/tls/private/${DOMAIN}.${OLD_YEAR}.key
-		unlink /etc/pki/tls/private/${DOMAIN}.${OLD_YEAR}.csr
-		unlink /etc/pki/tls/certs/${DOMAIN}.${OLD_YEAR}.crt
-		unlink /etc/pki/tls/certs/${DOMAIN_BUNDLE}.${OLD_YEAR}.crt
+if [ "${OPTARG}" == "1" ]; then
+		# Decommission old certificates.
+		unlink /etc/pki/tls/private/${DOMAIN}.key
+		unlink /etc/pki/tls/private/${DOMAIN}.csr
+		unlink /etc/pki/tls/certs/${DOMAIN}.crt
+		unlink /etc/pki/tls/certs/${SHORTNAME}-ca-bundle.crt
 
-		# @Commission new certificates.
+		# Commission new certificates.
 		ln -s /etc/pki/tls/private/${DOMAIN}.${NEW_YEAR}.key /etc/pki/tls/private/${DOMAIN_KEY}.key
 		ln -s /etc/pki/tls/private/${DOMAIN}.${NEW_YEAR}.csr /etc/pki/tls/private/${DOMAIN_CSR}.csr
 		ln -s /etc/pki/tls/certs/${DOMAIN}.${NEW_YEAR}.crt /etc/pki/tls/certs/${DOMAIN}.crt
-		ln -s /etc/pki/tls/certs/${DOMAIN_BUNDLE}.${NEW_YEAR}.crt /etc/pki/tls/certs/${DOMAIN_BUNDLE}.crt
+		ln -s /etc/pki/tls/certs/${SHORTNAME}-ca-bundle.${NEW_YEAR}.crt /etc/pki/tls/certs/${SHORTNAME}-ca-bundle.crt
 	else
 		echo "Moving on.."
 		#log_warning_msg "Error: Please check your requirements."
