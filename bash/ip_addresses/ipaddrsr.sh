a9604bd0bc47a4f268c87d87f76ab1121a13a7d4
hoodielive <hoodielive@gmail.com>
2021-02-26 13:03:56 -0500

    Parse IP addr script

---
+++ bash/ip_addresses/ipaddrsr.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+#set -x 
+function myip() 
+{
+    curl http://icanhazip.com
+
+    ip addr | grep inet$IP | \
+        cut -d"/" -f 1 | \
+        grep -v 127\.0 | \
+        grep -v \:\:1 | \
+        awk '{$1=$1};1'
+}
+
+myip
